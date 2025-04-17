30151fdac5d2b3d4f9dca4be80b609fea4d8fb99
Smaga Max <smaga.max@gmail.com>
2025-04-17 22:30:52 +0300

    add and configure application layer

---
+++ OnlineCoursePlatform.Application/Profiles/MappingProfiles.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace OnlineCoursePlatform.Application.Profiles
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            
+        }
+    }
+}
--- OnlineCoursePlatform.Domain/obj/Debug/net8.0/OnlineCoursePlatform.Domain.AssemblyInfo.cs
+++ OnlineCoursePlatform.Domain/obj/Debug/net8.0/OnlineCoursePlatform.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineCoursePlatform.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+769f3e447d26b3ad078c93c56454454c9a094acc")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineCoursePlatform.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineCoursePlatform.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
