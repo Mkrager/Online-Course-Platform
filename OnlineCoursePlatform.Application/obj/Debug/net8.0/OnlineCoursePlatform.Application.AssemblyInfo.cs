27fb0559491ea1de3e16bc3d84f33b8fc065b257
Smaga Max <smaga.max@gmail.com>
2025-04-22 17:01:05 +0300

    add some frontend

--- OnlineCoursePlatform.Application/obj/Debug/net8.0/OnlineCoursePlatform.Application.AssemblyInfo.cs
+++ OnlineCoursePlatform.Application/obj/Debug/net8.0/OnlineCoursePlatform.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineCoursePlatform.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d8ee4214eec440363db5890a4980294224c760d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c16193c0fe91892d8ed5df38dd161f315cb8701")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineCoursePlatform.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineCoursePlatform.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- OnlineCoursePlatfrom.Persistence.InregrationTests/obj/Debug/net8.0/OnlineCoursePlatfrom.Persistence.InregrationTests.AssemblyInfo.cs
+++ OnlineCoursePlatfrom.Persistence.InregrationTests/obj/Debug/net8.0/OnlineCoursePlatfrom.Persistence.InregrationTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineCoursePlatfrom.Persistence.InregrationTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d8ee4214eec440363db5890a4980294224c760d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c16193c0fe91892d8ed5df38dd161f315cb8701")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineCoursePlatfrom.Persistence.InregrationTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineCoursePlatfrom.Persistence.InregrationTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
