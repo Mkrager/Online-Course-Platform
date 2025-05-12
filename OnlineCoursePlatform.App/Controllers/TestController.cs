53d183f0eb35be30a41239c3c5ce7524429a691a
Smaga Max <smaga.max@gmail.com>
2025-05-12 20:57:43 +0300

    add createTest view

---
+++ OnlineCoursePlatform.App/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineCoursePlatform.App.Contracts;
+using OnlineCoursePlatform.App.ViewModels.Test;
+
+namespace OnlineCoursePlatform.App.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly ITestDataService _testDataService;
+
+        public TestController(ITestDataService testDataService)
+        {
+            _testDataService = testDataService;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(TestViewModel testViewModel)
+        {
+            var result = await _testDataService.CreateTest(testViewModel);
+
+            return View();
+        }
+    }
+}
--- OnlineCoursePlatform.App/obj/Debug/net8.0/OnlineCoursePlatform.App.AssemblyInfo.cs
+++ OnlineCoursePlatform.App/obj/Debug/net8.0/OnlineCoursePlatform.App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineCoursePlatform.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+817bbba6e4e70ba1e5e1e47ddef455de042967c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed143eab7274cd37ccf9b832eb33c202716cae22")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineCoursePlatform.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineCoursePlatform.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
