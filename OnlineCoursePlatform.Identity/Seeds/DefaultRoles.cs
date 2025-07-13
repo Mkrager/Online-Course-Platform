bf3770d948ae0cb2ab85fa144d26f98a122b3744
Smaga Max <smaga.max@gmail.com>
2025-07-13 16:05:44 +0300

    add check before create roles

--- OnlineCoursePlatform.Identity/Seeds/DefaultRoles.cs
+++ OnlineCoursePlatform.Identity/Seeds/DefaultRoles.cs
@@ -8,10 +8,17 @@ namespace OnlineCoursePlatform.Identity.Seeds
     {
         public static async Task SeedAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
-            await roleManager.CreateAsync(new IdentityRole(Roles.Admin));
-            await roleManager.CreateAsync(new IdentityRole(Roles.Default));
-            await roleManager.CreateAsync(new IdentityRole(Roles.Moderator));
-            await roleManager.CreateAsync(new IdentityRole(Roles.Teacher));
+            if (!await roleManager.RoleExistsAsync(Roles.Admin))
+                await roleManager.CreateAsync(new IdentityRole(Roles.Admin));
+
+            if (!await roleManager.RoleExistsAsync(Roles.Default))
+                await roleManager.CreateAsync(new IdentityRole(Roles.Default));
+
+            if (!await roleManager.RoleExistsAsync(Roles.Moderator))
+                await roleManager.CreateAsync(new IdentityRole(Roles.Moderator));
+
+            if (!await roleManager.RoleExistsAsync(Roles.Teacher))
+                await roleManager.CreateAsync(new IdentityRole(Roles.Teacher));
         }
     }
 }
