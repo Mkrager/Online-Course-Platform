cda46deca80e8d5712dc1f46e09ece2dbbaa9397
Smaga Max <smaga.max@gmail.com>
2025-07-16 14:05:26 +0300

    implement CreateEnrollmentCommandHandler

--- OnlineCoursePlatform.Application/Features/Enrollments/Commands/CreateEnrollment/CreateEnrollmentCommandHandler.cs
+++ OnlineCoursePlatform.Application/Features/Enrollments/Commands/CreateEnrollment/CreateEnrollmentCommandHandler.cs
@@ -1,26 +1,33 @@
-﻿//using AutoMapper;
-//using MediatR;
-//using OnlineCoursePlatform.Application.Contracts.Persistance;
-//using OnlineCoursePlatform.Domain.Entities;
+﻿using AutoMapper;
+using MediatR;
+using OnlineCoursePlatform.Application.Contracts;
+using OnlineCoursePlatform.Application.Contracts.Persistance;
+using OnlineCoursePlatform.Domain.Entities;
 
-//namespace OnlineCoursePlatform.Application.Features.Enrollments.Commands.CreateEnrollment
-//{
-//    public class CreateEnrollmentCommandHandler : IRequestHandler<CreateEnrollmentCommand, Guid>
-//    {
-//        private readonly IAsyncRepository<Enrollment> _enrollmentRepository;
-//        private readonly IMapper _mapper;
-//        public CreateEnrollmentCommandHandler(IAsyncRepository<Enrollment> enrollmentRepository, IMapper mapper)
-//        {
-//            _enrollmentRepository = enrollmentRepository;
-//            _mapper = mapper;
-//        }
+namespace OnlineCoursePlatform.Application.Features.Enrollments.Commands.CreateEnrollment
+{
+    public class CreateEnrollmentCommandHandler : IRequestHandler<CreateEnrollmentCommand, Guid>
+    {
+        private readonly IAsyncRepository<Enrollment> _enrollmentRepository;
+        private readonly ICurrentUserService _currentUserService;
+        private readonly IMapper _mapper;
+        public CreateEnrollmentCommandHandler(IAsyncRepository<Enrollment> enrollmentRepository, IMapper mapper, ICurrentUserService currentUserService)
+        {
+            _enrollmentRepository = enrollmentRepository;
+            _mapper = mapper;
+            _currentUserService = currentUserService;
+        }
 
-//        public async Task<Guid> Handle(CreateEnrollmentCommand request, CancellationToken cancellationToken)
-//        {
-//            var enrollmentToCreate = _mapper.Map<Enrollment>(request);
-//            var enrollment = await _enrollmentRepository.AddAsync(enrollmentToCreate);
+        public async Task<Guid> Handle(CreateEnrollmentCommand request, CancellationToken cancellationToken)
+        {
+            var enrollmentToCreate = _mapper.Map<Enrollment>(request);
 
-//            return enrollment.Id;
-//        }
-//    }
-//}
+            enrollmentToCreate.EnrolledAt = DateTime.UtcNow;
+            enrollmentToCreate.StudentId = _currentUserService.UserId;
+
+            var enrollment = await _enrollmentRepository.AddAsync(enrollmentToCreate);
+
+            return enrollment.Id;
+        }
+    }
+}
