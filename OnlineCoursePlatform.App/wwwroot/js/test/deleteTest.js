5a6d1b3ad3f5ac8f959cc9709f75909c043e35ea
Smaga Max <smaga.max@gmail.com>
2025-08-04 16:31:22 +0300

    spli deleteLesson.js

---
+++ OnlineCoursePlatform.App/wwwroot/js/test/deleteTest.js
@@ -0,0 +1,14 @@
+﻿function deleteTest(testId) {
+    $.ajax({
+        url: '/test/delete/' + testId,
+        type: 'DELETE',
+        dataType: 'json',
+        success: function (response) {
+            window.location.href = response.redirectUrl;
+        },
+        error: function (error) {
+            console.error('Error:', error);
+            alert('Error');
+        }
+    });
+}
