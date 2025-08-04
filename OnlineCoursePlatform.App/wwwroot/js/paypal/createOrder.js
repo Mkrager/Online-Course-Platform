e2f294da4914fb8ea0076465c550dda4b2d8fc5c
Smaga Max <smaga.max@gmail.com>
2025-08-04 16:25:36 +0300

    split createOrder in js file

---
+++ OnlineCoursePlatform.App/wwwroot/js/paypal/createOrder.js
@@ -0,0 +1,21 @@
+﻿function createOrder(courseId) {
+    $.ajax({
+        url: '/payPal/createOrder?courseId=' + courseId,
+        type: 'POST',
+        success: function (result) {
+            if (result.isSuccess) {
+                window.open(
+                    result.data,
+                    '_blank',
+                    'width=1000,height=800,top=100,left=200,toolbar=no,menubar=no,scrollbars=yes,resizable=yes'
+                );
+            } else {
+                alert('Error: ' + result.errorText);
+            }
+        },
+        error: function (xhr, status, error) {
+            console.error('AJAX Error:', error);
+            alert('Error occured');
+        }
+    });
+}
