d7e93425039e75af66f630500f36ee01fc638304
Smaga Max <smaga.max@gmail.com>
2025-08-04 16:11:48 +0300

    split js script from view

---
+++ OnlineCoursePlatform.App/wwwroot/js/updateCourse.js
@@ -0,0 +1,26 @@
+﻿function updateCourse() {
+    const form = document.getElementById('updateCourseForm');
+    const formData = new FormData(form);
+
+    $.ajax({
+        url: '/course/update',
+        type: 'PUT',
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function (response) {
+            if (response.redirectToUrl) {
+                window.location.href = response.redirectToUrl;
+            }
+            else {
+                for (var key in response.errors) {
+                    var errorMessage = response.errors[key];
+                    $('#' + key).next('.error-text').text(errorMessage);
+                }
+            }
+        },
+        error: function (xhr) {
+            console.log(xhr.responseText);
+        }
+    });
+}
