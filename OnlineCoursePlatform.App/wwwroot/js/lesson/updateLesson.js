ca534d24609d0623102313bc74d25a4106a95cfa
Smaga Max <smaga.max@gmail.com>
2025-08-04 16:18:07 +0300

    splt update lesson to js file

---
+++ OnlineCoursePlatform.App/wwwroot/js/lesson/updateLesson.js
@@ -0,0 +1,26 @@
+﻿function updateLesson() {
+    const form = document.getElementById('updateLessonForm');
+    const formData = new FormData(form);
+
+    $.ajax({
+        url: '@Url.Action("Update", "Lesson")',
+        type: 'PUT',
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function (response) {
+            if (response.redirectToUrl) {
+                window.location.href = response.redirectToUrl;
+            }
+            else {
+                for (var key in response.errors) {
+                    var errorMessage = response.errors[key];
+                    $('#' + key).next('.error-text').text(errorMessage);
+                }
+            }
+        },
+        error: function (xhr) {
+            console.log(xhr.responseText);
+        }
+    });
+}
