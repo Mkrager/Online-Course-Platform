692e160e5fc937b0bff70600cc6a988620f821b1
Smaga Max <smaga.max@gmail.com>
2025-08-04 16:15:50 +0300

    split deleteLesson script

---
+++ OnlineCoursePlatform.App/wwwroot/js/lesson/deleteLesson.js
@@ -0,0 +1,14 @@
+﻿function deleteLesson(lessonId) {
+    $.ajax({
+        url: '/lesson/delete/' + lessonId,
+        type: 'DELETE',
+        dataType: 'json',
+        success: function (response) {
+            window.location.href = response.redirectUrl;
+        },
+        error: function (error) {
+            console.error('Error:', error);
+            alert('Error');
+        }
+    });
+}
