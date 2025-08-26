194fdc9f5f51436754f34a6ae185ceb4946bc1f2
Smaga Max <smaga.max@gmail.com>
2025-08-26 19:29:55 +0300

    implement UpdateStatusAsync

---
+++ OnlineCoursePlatform.Application/Contracts/Persistance/ICoursePublishRequestRepository.cs
@@ -0,0 +1,10 @@
+﻿using OnlineCoursePlatform.Domain.Entities;
+using OnlineCoursePlatform.Domain.Enums;
+
+namespace OnlineCoursePlatform.Application.Contracts.Persistance
+{
+    public interface ICoursePublishRequestRepository
+    {
+        Task UpdateStatusAsync(CoursePublishRequest coursePublishRequest, CoursePublishStatus newStatus);
+    }
+}
---
+++ OnlineCoursePlatform.Persistence/Repositories/CoursePublishRequestRepository.cs
@@ -0,0 +1,19 @@
+﻿using OnlineCoursePlatform.Application.Contracts.Persistance;
+using OnlineCoursePlatform.Domain.Entities;
+using OnlineCoursePlatform.Domain.Enums;
+
+namespace OnlineCoursePlatform.Persistence.Repositories
+{
+    public class CoursePublishRequestRepository : BaseRepository<CoursePublishRequestRepository>, ICoursePublishRequestRepository
+    {
+        public CoursePublishRequestRepository(OnlineCoursePlatformDbContext dbContext) : base(dbContext)
+        {      
+        }
+
+        public async Task UpdateStatusAsync(CoursePublishRequest coursePublishRequest, CoursePublishStatus newStatus)
+        {
+            coursePublishRequest.Status = newStatus;
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
