956b099920ce742c3854fca419a4b3be8a9e7405
Smaga Max <smaga.max@gmail.com>
2025-09-11 18:09:18 +0300

    fix list course request view

--- OnlineCoursePlatform.App/ViewModels/CoursePublishRequest/CoursePublishRequestListViewModel.cs
+++ OnlineCoursePlatform.App/ViewModels/CoursePublishRequest/CoursePublishRequestListViewModel.cs
@@ -5,7 +5,9 @@
         public Guid CourseId { get; set; }
         public string RequestedBy { get; set; } = string.Empty;
         public DateTime RequestedDate { get; set; }
+        public string RequestedName { get; set; } = string.Empty;
         public string ApprovedBy { get; set; } = string.Empty;
+        public string ApprovedName { get; set; } = string.Empty;
         public DateTime ApprovedAt { get; set; }
         public CoursePublishStatus Status { get; set; }
     }
