e96fd3c6e7a6c4d55ff44732c6bca5915332f37a
Smaga Max <smaga.max@gmail.com>
2025-09-18 17:35:21 +0300

    refactor UserDataService

--- OnlineCoursePlatform.App/Services/UserDataServcie.cs
+++ OnlineCoursePlatform.App/Services/UserDataServcie.cs
@@ -1,35 +1,19 @@
 ﻿using OnlineCoursePlatform.App.Contracts;
+using OnlineCoursePlatform.App.Infrastructure.BaseServices;
 using OnlineCoursePlatform.App.ViewModels.User;
-using System.Net.Http.Headers;
 using System.Text.Json;
 
 namespace OnlineCoursePlatform.App.Services
 {
-    public class UserDataServcie : IUserDataService
+    public class UserDataServcie : BaseDataService, IUserDataService
     {
-        private readonly HttpClient _httpClient;
-        private readonly JsonSerializerOptions _jsonOptions;
-        private readonly IAuthenticationService _authenticationService;
-
-        public UserDataServcie(HttpClient httpClient, IAuthenticationService authenticationService)
+        public UserDataServcie(IHttpClientFactory httpClientFactory) : base(httpClientFactory)
         {
-            _httpClient = httpClient;
-            _jsonOptions = new JsonSerializerOptions
-            {
-                PropertyNameCaseInsensitive = true
-            };
-            _authenticationService = authenticationService;
         }
 
         public async Task<UserDetailsResponse> GetDefaultUserDetailsAsync()
         {
-            var request = new HttpRequestMessage(HttpMethod.Get, $"https://localhost:7275/api/user/default");
-
-            var accessToken = _authenticationService.GetAccessToken();
-
-            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-
-            var response = await _httpClient.SendAsync(request);
+            var response = await _httpClient.GetAsync("user/default");
 
             if (response.IsSuccessStatusCode)
             {
@@ -45,13 +29,7 @@ namespace OnlineCoursePlatform.App.Services
 
         public async Task<UserDetailsResponse> GetTeacherDetailsAsync()
         {
-            var request = new HttpRequestMessage(HttpMethod.Get, $"https://localhost:7275/api/user/teacher");
-
-            var accessToken = _authenticationService.GetAccessToken();
-
-            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-
-            var response = await _httpClient.SendAsync(request);
+            var response = await _httpClient.GetAsync("user/teacher");
 
             if (response.IsSuccessStatusCode)
             {
@@ -63,7 +41,6 @@ namespace OnlineCoursePlatform.App.Services
             }
 
             return new UserDetailsResponse();
-
         }
     }
 }
