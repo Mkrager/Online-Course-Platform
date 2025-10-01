ec40f768401aa4135ec3d00dfb2edd8cb19226c7
Smaga Max <smaga.max@gmail.com>
2025-10-01 23:58:23 +0300

    implemet teacherApplication UpdateStatusAsync

---
+++ OnlineCoursePlatform.Application/Contracts/Persistance/ITeacherApplicationRepository.cs
@@ -0,0 +1,10 @@
+﻿using OnlineCoursePlatform.Domain.Entities;
+using OnlineCoursePlatform.Domain.Enums;
+
+namespace OnlineCoursePlatform.Application.Contracts.Persistance
+{
+    public interface ITeacherApplicationRepository : IAsyncRepository<TeacherApplication>
+    {
+        Task UpdateStatusAsync(TeacherApplication teacherApplication, RequestStatus newStatus, string? rejectReason = null);
+    }
+}
--- OnlineCoursePlatform.Application/Features/CoursePublishRequests/Commands/UpdateCoursePublishRequestStatus/ApproveCourse/ApproveCoursePublishRequestCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace OnlineCoursePlatform.Application.Features.CoursePublishRequests.Commands.UpdateCoursePublishRequestStatus.ApproveCourse
-{
-    public class ApproveCoursePublishRequestCommand : IRequest
-    {
-        public Guid Id { get; set; }
-    }
-}
