b1067f1f295fd262c3eed3e7ea2b89fe469d5260
Smaga Max <smaga.max@gmail.com>
2025-10-02 21:44:21 +0300

    implement BaseUpdateRequestStatusCommandHandler

---
+++ OnlineCoursePlatform.Application/Common/Handlers/Commands/BaseUpdateRequestStatusCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using OnlineCoursePlatform.Application.Contracts.Persistance;
+using OnlineCoursePlatform.Application.Exceptions;
+using OnlineCoursePlatform.Domain.Common;
+
+namespace OnlineCoursePlatform.Application.Common.Handlers.Commands
+{
+    public abstract class BaseUpdateRequestStatusCommandHandler<TEntity, TCommand>
+        : IRequestHandler<TCommand>
+        where TEntity : RequestEntity
+        where TCommand : IRequest<Unit>
+    {
+        private readonly IRequestRepository<TEntity> _repository;
+        protected BaseUpdateRequestStatusCommandHandler(IRequestRepository<TEntity> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Unit> Handle(TCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _repository.GetByIdAsync(GetId(request));
+
+            if (entity == null)
+                throw new NotFoundException(typeof(TEntity).Name, GetId(request));
+
+            await HandleRequestAsync(entity, request, cancellationToken);
+
+            return Unit.Value;
+        }
+
+        protected abstract Guid GetId(TCommand request);
+
+        protected abstract Task HandleRequestAsync(TEntity entity, TCommand request, CancellationToken cancellationToken);
+
+        protected Task UpdateStatusAsync(TEntity entity, Domain.Enums.RequestStatus status, string? reason = null)
+        {
+            entity.Status = status;
+            entity.RejectReason = reason;
+            return _repository.UpdateStatusAsync(entity, status, reason);
+        }
+    }
+}
