210cf9233287b4c34f5086d9d43fd8d5eb51e832
Smaga Max <smaga.max@gmail.com>
2025-10-03 17:14:09 +0300

    implement CreateTeacherApplicationCommandHandler

--- OnlineCoursePlatform.Application/Features/TeacherApplications/Commands/CreateTeacherApplication/CreateTeacherApplicationCommandHandler.cs
+++ OnlineCoursePlatform.Application/Features/TeacherApplications/Commands/CreateTeacherApplication/CreateTeacherApplicationCommandHandler.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using OnlineCoursePlatform.Application.Common.Handlers.Commands;
+using OnlineCoursePlatform.Application.Contracts.Persistance;
+using OnlineCoursePlatform.Domain.Entities;
 
 namespace OnlineCoursePlatform.Application.Features.TeacherApplications.Commands.CreateTeacherApplication
 {
-    internal class CreateTeacherApplicationCommandHandler
+    public class CreateTeacherApplicationCommandHandler : CreateEntityCommandHandler<CreateTeacherApplicationCommand, TeacherApplication, Guid>
     {
+        public CreateTeacherApplicationCommandHandler(IAsyncRepository<TeacherApplication> repository, IMapper mapper) : base(repository, mapper)
+        {
+        }
+
+        protected override Guid BuildResponse(TeacherApplication entity) => entity.Id;
     }
 }
