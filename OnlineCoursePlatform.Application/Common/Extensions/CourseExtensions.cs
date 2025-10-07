8b9b3e454022769fc5f64d5cc89bf366639101b4
Smaga Max <smaga.max@gmail.com>
2025-10-07 17:56:27 +0300

    implement CourseExtensions

---
+++ OnlineCoursePlatform.Application/Common/Extensions/CourseExtensions.cs
@@ -0,0 +1,10 @@
+﻿using OnlineCoursePlatform.Domain.Entities;
+
+namespace OnlineCoursePlatform.Application.Common.Extensions
+{
+    public static class CourseExtensions
+    {
+        public static bool IsCreatedBy(this Course course, string userId)
+            => course.CreatedBy == userId;
+    }
+}
