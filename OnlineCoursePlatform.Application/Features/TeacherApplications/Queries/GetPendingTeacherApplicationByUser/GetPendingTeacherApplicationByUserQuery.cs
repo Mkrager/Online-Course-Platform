74ad13503e29c281b708e4db64a4b75ab045d1a5
Smaga Max <smaga.max@gmail.com>
2025-10-12 20:11:55 +0300

    implement GetPendingTeacherApplicationByUser

---
+++ OnlineCoursePlatform.Application/Features/TeacherApplications/Queries/GetPendingTeacherApplicationByUser/GetPendingTeacherApplicationByUserQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using OnlineCoursePlatform.Application.Features.TeacherApplications.Queries.GetTeacherApplicationLIst;
+
+namespace OnlineCoursePlatform.Application.Features.TeacherApplications.Queries.GetPendingTeacherApplicationByUser
+{
+    public class GetPendingTeacherApplicationByUserQuery : IRequest<List<TeacherApplicationListVm>>
+    {
+        public string UserId { get; set; } = string.Empty;
+    }
+}
---
+++ OnlineCoursePlatform.Application/Features/TeacherApplications/Queries/GetPendingTeacherApplicationByUser/GetPendingTeacherApplicationByUserQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MediatR;
+using OnlineCoursePlatform.Application.Contracts.Persistance;
+using OnlineCoursePlatform.Application.Features.TeacherApplications.Queries.GetTeacherApplicationLIst;
+using OnlineCoursePlatform.Domain.Entities;
+using OnlineCoursePlatform.Domain.Enums;
+
+namespace OnlineCoursePlatform.Application.Features.TeacherApplications.Queries.GetPendingTeacherApplicationByUser
+{
+    public class GetPendingTeacherApplicationByUserQueryHandler : IRequestHandler<GetPendingTeacherApplicationByUserQuery, List<TeacherApplicationListVm>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IRequestRepository<TeacherApplication> _teacherApplicationRepository;
+        public GetPendingTeacherApplicationByUserQueryHandler(IMapper mapper, IRequestRepository<TeacherApplication> teacherApplicationRepository)
+        {
+            _mapper = mapper;
+            _teacherApplicationRepository = teacherApplicationRepository;
+        }
+        public async Task<List<TeacherApplicationListVm>> Handle(GetPendingTeacherApplicationByUserQuery request, CancellationToken cancellationToken)
+        {
+            var teacherApplications = await _teacherApplicationRepository.GetRequestsByUserIdAndStatusAsync(request.UserId, RequestStatus.Pending);
+            return _mapper.Map<List<TeacherApplicationListVm>>(teacherApplications);
+        }
+    }
+}
