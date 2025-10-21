3d868c22ff794319525f9c5f7df65b1ffe7710db
Smaga Max <smaga.max@gmail.com>
2025-10-21 18:11:47 +0300

    implement CreateSupportTicketCommandValidator

---
+++ OnlineCoursePlatform.Application/Features/SupportTickets/Commands/CreateSupportTicket/CreateSupportTicketCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace OnlineCoursePlatform.Application.Features.SupportTickets.Commands.CreateSupportTicket
+{
+    public class CreateSupportTicketCommandValidator : AbstractValidator<CreateSupportTicketCommand>
+    {
+        public CreateSupportTicketCommandValidator()
+        {
+            RuleFor(r => r.Subject)
+                .NotEmpty().WithMessage("Subject is required.");
+
+            RuleFor(r => r.Message)
+                .NotEmpty().WithMessage("Message is required.");
+        }
+    }
+}
